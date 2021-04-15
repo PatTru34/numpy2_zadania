9bdeb7baaa1efcc0e747b6bfe79640897e0bbd7e
PatTru34 <PatTru34>
2021-04-15 15:10:16 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+# Zad 1
+# a=np.arange(3)
+# b=np.arange(4,10,2)
+#
+# print(a)
+# print(b)
+# c=a*b
+# print(c)
+
+# Zad 2
+# a=np.arange(9).reshape(3,3)
+# b=np.array([[4,5,2,7],[1,7,8,8],[6,11,2,4],[7,2,4,1]])
+# print(a)
+# print(a.min(axis=1))
+# print(a.min(axis=0))
+# print(b)
+# print(b.min(axis=0))
+# print(b.min(axis=1))
+
+# Zad 3
+# a=np.arange(3)
+# b=np.arange(4,10,2)
+#
+# print(np.dot(a,b))
+# Zad 4
+# a=np.arange(3)
+# b=np.arange(3.,6.,1)
+# print(a)
+# print(b)
+# c=a*b
+# print(c)
+
+# Zad 5
+# b=np.array([[2,3],[6,3],[8,8]])
+# a=np.sin(b)
+# print(b)
+# print(a)
+
+# Zad 6
+# b=np.array([[6,10],[2,5],[4,8]])
+# a=np.cos(b)
+# print(b)
+# print(a)
+
+# Zad 7
+# a=np.arange(6).reshape(3,2)
+# b=np.array([[3,5],[6,1],[7,4]])
+# print(a)
+# print(b)
+# c=a+b
+# print(c)
+
+# Zad 8
+# a=np.arange(9).reshape(3,3)
+# print(a)
+# for b in a:
+#     print(b)
+#     print("")
+# Zad 9
+# a=np.array([[3,5,3],[4,8,6],[9,9,9]])
+# for b in a.flat:
+#     print(b)
+# Zad 10
+# a=np.arange(81).reshape(9,9)
+# # mozemy wybrac tylko mozliwoci w ktorych iloczyc wierszy i kolumn bedzie wynosila 81
+# b=a.reshape(81,1)
+# c=a.reshape(1,81)
+# d=a.reshape(27,3)
+# e=a.reshape(3,27)
+
+# Zad 11
+# a=np.arange(12).reshape(3,4)
+# b=np.arange(12).reshape(4,3)
+# c=np.arange(12).reshape(2,6)
+# print(a)
+# print(b)
+# print(c)
+# print(a.ravel())
+# print(b.ravel())
+# print(c.ravel())
+
